2bc7b5b3af808535a1a82ed5f55827bdb29e4d1d
neducod <nedunony16@gmail.com>
2025-04-04 22:40:22 +0100

    Updates css, modified web pages

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ AOS.init({
     duration: 1200, // Animation duration in milliseconds
     once: true,     // Whether animation should happen only once (set to false to repeat on scroll)
   });
+
+
+
+//POPUP FOR ABOUT.HTML
+  // Automatically show popup when page loads
+  window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+  };
+
+  function goBack() {
+    // Go to previous page
+    window.history.back();
+    
+    // OR reload the current page:
+    // window.location.reload();
+    
+    // OR redirect to a specific page:
+    // window.location.href = "https://example.com";
+  }
